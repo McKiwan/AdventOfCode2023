1196a7c4c9f41a3838117957942e345c02342ed9
McKiwan <xavier.ledu1@gmail.com>
2023-12-07 18:42:59 +0100

    Finished Day 6

---
+++ Day6/Part1.py
@@ -0,0 +1,34 @@
+# ADVENT OF CODE 2023
+# --- Day 6: Wait For It ---
+# https://adventofcode.com/2023/day/6
+# --- Part One ---
+# Read the input file
+with open("input.txt") as f:
+    data = f.read().strip()
+    f.close()
+
+# Split the data to put time and distance in 2 different lists
+data = data.strip().split("\n")
+data = [x.strip().split(":")[1:] for x in data]
+for i, x in enumerate(data):
+    data[i] = [y.strip().split() for y in x]
+    data[i] = [int(y) for y in data[i][0]]
+times = data[0]
+distances = data[1]
+
+# Calculate the number of differents ways to beat the record for each time
+ans = 1
+for i, time in enumerate(times):
+    x = 0
+    total = 0
+    while x < time:
+        timeleft = time - x
+        if (x * timeleft) > distances[i]:
+            total += 1
+        x += 1
+    print(
+        f"There is {total} differents way to beat the record of {distances[i]} millimeters"
+    )
+    ans *= total
+
+print(ans)
---
+++ Day6/Part2.py
@@ -0,0 +1,41 @@
+# ADVENT OF CODE 2023
+# --- Day 6: Wait For It ---
+# https://adventofcode.com/2023/day/6#part2
+# --- Part Two ---
+# Read the input file
+with open("input.txt") as f:
+    data = f.read().strip()
+    f.close()
+
+# Split the data to remove text and put time and distance in 2 different variables
+data = data.strip().split("\n")
+data = [x.strip().split(":")[1:] for x in data]
+for i, x in enumerate(data):
+    data[i] = [y.strip().split() for y in x]
+    data[i] = [y for y in data[i][0]]
+
+# There is only 1 time and 1 distance
+# So we had to concatenate the 2 lists of str to have 1 variable for each
+time = ""
+for x in data[0]:
+    time = time + x
+time = int(time)
+print(time)
+
+distance = ""
+for x in data[1]:
+    distance = distance + x
+distance = int(distance)
+print(distance)
+
+# Calculate the number of differents ways to beat the record
+x = 0
+total = 0
+while x < time:
+    timeleft = time - x
+    if (x * timeleft) > distance:
+        total += 1
+    x += 1
+print(f"There is {total} differents way to beat the record of {distance} millimeters")
+
+print(total)
